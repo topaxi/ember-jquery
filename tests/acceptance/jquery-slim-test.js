d80d49bfadaf7f598ec07a779bbb24ffb036d704
Damian Senn <senn@puzzle.ch>
2017-04-13 08:55:50 +0200

    Test that only one instance of jQuery is loaded

--- tests/acceptance/jquery-slim-test.js
+++ tests/acceptance/jquery-slim-test.js
@@ -5,12 +5,18 @@ import moduleForAcceptance from '../../tests/helpers/module-for-acceptance'
 
 moduleForAcceptance('Acceptance | jquery slim')
 
+const JQUERY_VERSION = '3.2.1 -ajax,-ajax/jsonp,-ajax/load,-ajax/parseXML,-ajax/script,-ajax/var/location,-ajax/var/nonce,-ajax/var/rquery,-ajax/xhr,-manipulation/_evalUrl,-event/ajax,-effects,-effects/Tween,-effects/animatedSelector'
+
 test('Comparing jQuery version', function(assert) {
   visit('/')
 
   andThen(function() {
     assert.equal(currentURL(), '/')
-    assert.equal($.fn.jquery, '3.2.1 -ajax,-ajax/jsonp,-ajax/load,-ajax/parseXML,-ajax/script,-ajax/var/location,-ajax/var/nonce,-ajax/var/rquery,-ajax/xhr,-manipulation/_evalUrl,-event/ajax,-effects,-effects/Tween,-effects/animatedSelector')
-    assert.equal(Ember.$.fn.jquery, $.fn.jquery)
+    assert.equal($.fn.jquery, JQUERY_VERSION)
+    assert.equal(Ember.$.fn.jquery, JQUERY_VERSION)
+    assert.equal(Ember.$, $)
+    assert.equal(window.$, $)
+    $.noConflict()
+    assert.equal(window.$, undefined)
   })
 })
